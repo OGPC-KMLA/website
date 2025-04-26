775521314adb82c0688745db328c9378e3fd2aa0
OGPC-KMLA <devgobrowse@gmail.com>
2025-04-26 11:44:00 -0700

    Update conf.py
    adding credits

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Super Storm Bros'
+copyright = '2025, Mateo A'
+author = 'Mateo A'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.3'
 
 # -- General configuration
 
